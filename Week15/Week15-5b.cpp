8738fcc7a15ab066b14e26f4b9684aeed1c9895a
Ting1013 <13750830@me.mcu.edu.tw>
2026-06-03 11:39:16 +0800

    Week15

---
+++ Week15/Week15-5b.cpp
@@ -0,0 +1,23 @@
+// Week15-5b.cpp
+// SOIT108_Advance_012
+
+#include <stdio.h>
+
+int main()
+{
+    int K;
+    scanf("%d", &K);
+
+    int total = 0;
+
+    for (int i = 1; i <= 1000; i++) {
+        total += i;
+
+        if (total > K) {
+            printf("%d", i);
+            break;
+        }
+    }
+
+    return 0;
+}
---
+++ Week15/Week15-5c.cpp
@@ -0,0 +1,20 @@
+// Week15-5c.cpp
+// SOIT108_Advance_014
+
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int total = 0;
+
+    for (int i = 0; i <= N; i++) {
+        total += 2 * i + 1;
+    }
+
+    printf("f(%d)=%d", N, total);
+
+    return 0;
+}
